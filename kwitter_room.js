1b926b55e03185d4fc73acaf7591f0fad5536235
MUTO <75616805+MUTO2610@users.noreply.github.com>
2022-01-29 13:51:13 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
